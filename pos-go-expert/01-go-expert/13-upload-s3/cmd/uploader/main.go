72242f2cd603559ec0c31786bd617d1a7b2e25e4
Vitor Camargo <vitorlrrcamargo@gmail.com>
2025-01-27 09:47:42 -0300

    create 13-upload-s3 folder

---
+++ pos-go-expert/01-go-expert/13-upload-s3/cmd/uploader/main.go
@@ -0,0 +1,103 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"sync"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/credentials"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
+)
+
+var (
+	s3Client *s3.S3
+	s3Bucket string
+	wg       sync.WaitGroup
+)
+
+func init() {
+	sess, err := session.NewSession(
+		&aws.Config{
+			Region: aws.String("us-east-1"),
+			Credentials: credentials.NewStaticCredentials(
+				"---",
+				"---",
+				"",
+			),
+		},
+	)
+	if err != nil {
+		panic(err)
+	}
+	s3Client = s3.New(sess)
+	s3Bucket = "goexpert-bucket"
+}
+
+func main() {
+	dir, err := os.Open("./tmp")
+	if err != nil {
+		panic(err)
+	}
+	defer dir.Close()
+	uploadControl := make(chan struct{}, 100)
+	errorFileUpload := make(chan string, 10)
+
+	go func() {
+		for {
+			select {
+			case fileName := <-errorFileUpload:
+				uploadControl <- struct{}{}
+				wg.Add(1)
+				go uploadFile(fileName, uploadControl, errorFileUpload)
+			}
+		}
+	}()
+
+	for {
+		files, err := dir.Readdir(1)
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+			fmt.Printf("Error reading directory: %v\n", err)
+			continue
+		}
+		wg.Add(1)
+		uploadControl <- struct{}{}
+		uploadFile(files[0].Name(), uploadControl, errorFileUpload)
+	}
+	wg.Wait()
+}
+
+func uploadFile(fileName string, uploadControl <-chan struct{}, errorFileUpload chan<- string) {
+	defer wg.Done()
+	completeFileName := fmt.Sprintf("./tmp/%s", fileName)
+	fmt.Printf("Uploading file %s to bucket %s\n", completeFileName, s3Bucket)
+
+	f, err := os.Open(completeFileName)
+	if err != nil {
+		fmt.Printf("Error opening file %s: %v\n", completeFileName, err)
+		<-uploadControl
+		errorFileUpload <- fileName
+		return
+	}
+	defer f.Close()
+
+	_, err = s3Client.PutObject(&s3.PutObjectInput{
+		Bucket: aws.String(s3Bucket),
+		Key:    aws.String(fileName),
+		Body:   f,
+	})
+	if err != nil {
+		fmt.Printf("Error uploading file %s: %v\n", completeFileName, err)
+		<-uploadControl
+		errorFileUpload <- fileName
+		return
+	}
+
+	fmt.Printf("File %s uploaded successfully\n", completeFileName)
+	<-uploadControl
+}
---
+++ pos-go-expert/01-go-expert/13-upload-s3/go.mod
@@ -0,0 +1,7 @@
+module github.com/vitorlrrcamargo/full-cycle/pos-go-expert/01-go-expert/13-upload-s3
+
+go 1.23.2
+
+require github.com/aws/aws-sdk-go v1.55.6
+
+require github.com/jmespath/go-jmespath v0.4.0 // indirect
